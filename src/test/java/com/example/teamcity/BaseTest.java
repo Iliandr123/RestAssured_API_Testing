bffa4bf091921830503ce7bb80e367892712aa8a
Iliandr123 <iliascorpion124@gmail.com>
2024-11-03 22:15:05 +0500

    feat(ui tests): add supporting of ui tests

--- src/test/java/com/example/teamcity/BaseTest.java
+++ src/test/java/com/example/teamcity/BaseTest.java
@@ -1,4 +1,4 @@
-package com.example.teamcity.api;
+package com.example.teamcity;
 
 import com.example.teamcity.api.generators.TestDataStorage;
 import com.example.teamcity.api.models.TestData;
@@ -8,6 +8,8 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.asserts.SoftAssert;
 
+import static com.example.teamcity.api.enums.Endpoint.BUILD_TYPES;
+import static com.example.teamcity.api.enums.Endpoint.PROJECTS;
 import static com.example.teamcity.api.generators.TestDataGenerator.generate;
 
 public class BaseTest {
